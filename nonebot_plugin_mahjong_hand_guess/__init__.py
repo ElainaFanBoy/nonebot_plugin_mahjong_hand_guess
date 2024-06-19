245a679a13390edeffef23da057c001823044503
Nanako <56375835+ElainaFanBoy@users.noreply.github.com>
2024-06-19 23:37:10 +0800

    Update __init__.py

--- nonebot_plugin_mahjong_hand_guess/__init__.py
+++ nonebot_plugin_mahjong_hand_guess/__init__.py
@@ -1,6 +1,3 @@
-from nonebot_plugin_session import EventSession, SessionIdType
-from nonebot_plugin_alconna import UniMsg, Command, UniMessage, MsgTarget
-from nonebot_plugin_waiter import waiter
 from pathlib import Path
 
 from nonebot.adapters import Event
